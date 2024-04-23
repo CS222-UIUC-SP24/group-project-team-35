6bb0568fa2001f90cd087e4aa69e5c139f98aaf3
espageti <darreeb.lin@gmail.com>
2024-04-23 16:36:50 -0500

    remove song files on ready
    should be mostly done now

--- FFMpeg test/main.py
+++ FFMpeg test/main.py
@@ -22,6 +22,8 @@ import json
 
 from collections import namedtuple 
 
+import shutil
+
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -42,7 +44,12 @@ yt_opts = {
     
 }
 
-
+@bot.event  
+async def on_ready():
+    print("ready. Deleting all previous files")
+    shutil.rmtree("songs")
+    
+    os.makedirs("songs")
 
 
 @bot.command()
@@ -125,10 +132,14 @@ async def remove(ctx, queueN):
 async def deleteSong(song: SongFile):
     os.remove(song.fileName)
 
-#in the final implementation, should probably first search for the song on Spotify and show it to the user, so they can choose it. 
-#It'll then search by the proper name on Spotify
+
+
 @bot.command()
-async def play(ctx, *, search):
+async def playYT(ctx, *, search):
+    await   play(ctx, search)
+    
+    
+async def play(ctx, search):
     # Check if the user is in a voice channel
     if ctx.author.voice is None:
         await ctx.send("You need to be in a voice channel to use this command.")
@@ -149,7 +160,7 @@ async def play(ctx, *, search):
         return
     # Get the voice channel of the user
     voice_channel = ctx.author.voice.channel
-
+    
     while(len(queues[ctx.guild.id]) > 0):
         print("trying my best to play", len(queues[ctx.guild.id]))
         try:
@@ -168,9 +179,10 @@ async def play(ctx, *, search):
             while voice_client.is_playing() and len(voice_channel.members) > 1:
                 await asyncio.sleep(1)
             # Disconnect from the voice channel after the audio finishes playing. 
-            await voice_client.disconnect()
+            voice_client.disconnect()
             os.remove(nextSong.fileName)
             queues[ctx.guild.id].pop(0)
+            
         except Exception as e:
             print(e)
             await ctx.send("An error occurred while playing the audio.")
